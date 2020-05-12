2fb3aaccad001e888281d990aad8ad3fc20b4097
Arpit Bansal <saas@arpitbansal.com>
2020-05-12 11:00:32 +0800

    Added 'your-project' placeholder to files

--- server/bin/www.js
+++ server/bin/www.js
@@ -6,7 +6,7 @@
 import app from '../app';
 import debugLib from 'debug';
 import http from 'http';
-const debug = debugLib('dex-backend:server');
+const debug = debugLib('your-project:server');
 
 /**
  * Get port from environment and store in Express.
